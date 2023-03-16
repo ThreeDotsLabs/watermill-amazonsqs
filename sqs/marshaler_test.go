3ddddb938b1170e6b4ddb8292add4efd8f57feb6
Matt Palmer <9059517+56KBs@users.noreply.github.com>
2023-03-16 15:59:07 +0000

    fix(sqs): UUID lost via marshalling
    The watermill message UUID was lost during publishing & subscribing to the message, because sqs.Message's `MessageId` should signify the Amazon message ID, not the watermill message UUID.
    
    Use the metadata to store the UUID under the key `_watermill_message_uuid`.
    
    Minor simplifications/pre-allocations of data for performance.

--- sqs/marshaler_test.go
+++ sqs/marshaler_test.go
@@ -4,19 +4,11 @@ import (
 	"testing"
 
 	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestMetadaConverter(t *testing.T) {
-	metadata := message.Metadata{
-		"name": "some-event-name",
-		"test": "other-value",
-	}
-
-	metadataOut := attributesToMetadata(metadataToAttributes(metadata))
-
-	require.Equal(t, metadata, metadataOut, "metadata after transformations should be the same")
-}
 
 func TestMarshaler(t *testing.T) {
 	msg := message.NewMessage("some-id", []byte("payload"))
@@ -29,11 +21,68 @@ func TestMarshaler(t *testing.T) {
 
 	awsMsg, err := dum.Marshal(msg)
 	require.NoError(t, err)
+	assert.Equal(t, &sqs.Message{
+		Body: aws.String("payload"),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"_watermill_message_uuid": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("some-id"),
+			},
+			"name": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("some-event-name"),
+			},
+			"test": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("other-value"),
+			},
+		},
+	}, awsMsg)
+}
 
-	msgOut, err := dum.Unmarshal(awsMsg)
+func TestUnmarshaler(t *testing.T) {
+	m := &DefaultMarshalerUnmarshaler{}
+
+	msg, err := m.Unmarshal(&sqs.Message{
+		Body: aws.String("payload"),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"_watermill_message_uuid": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("some-id"),
+			},
+			"name": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("some-event-name"),
+			},
+			"test": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: aws.String("other-value"),
+			},
+			"nil datatype": {
+				DataType:    nil,
+				StringValue: nil,
+			},
+			"nil stringvalue": {
+				DataType:    aws.String(AWSStringDataType),
+				StringValue: nil,
+			},
+			"numeric": {
+				DataType:    aws.String(AWSNumberDataType),
+				StringValue: aws.String("1234"),
+			},
+			"binary": {
+				DataType:    aws.String(AWSBinaryDataType),
+				BinaryValue: []byte("binary-data"),
+			},
+		},
+	})
 	require.NoError(t, err)
-
-	require.Equal(t, msg.UUID, msgOut.UUID)
-	require.Equal(t, msg.Payload, msgOut.Payload)
-	require.Equal(t, msg.Metadata, msgOut.Metadata)
+	assert.Equal(t, "some-id", msg.UUID)
+	assert.Equal(t, message.Payload("payload"), msg.Payload)
+	assert.Equal(t, message.Metadata{
+		"name":    "some-event-name",
+		"test":    "other-value",
+		"numeric": "1234",
+		"binary":  "binary-data",
+	}, msg.Metadata)
 }
